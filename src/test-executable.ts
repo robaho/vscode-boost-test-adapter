48283f4b29dc354316598b7319e64208cc69facc
npotravkin <potravkin@physics.msu.ru>
2026-09-14 08:23:08 +0300

    Fix Windows Boost.Test diagnostic location parsing

--- src/test-executable.ts
+++ src/test-executable.ts
@@ -27,8 +27,8 @@ export class TestExecutable {
     private readonly regexLeaveTestSuite = /^(.+): Leaving test suite "(\w+)"(?:; testing time: (\d+)(\w+))?$/;
     private readonly regexEnterTestCase = /^(.+): Entering test case "(\w+)"$/;
     private readonly regexLeaveTestCase = /^(.+): Leaving test case "(\w+)"(?:; testing time: (\d+)(\w+))?$/;
-    private readonly regexTestCaseError = /^(.+):?\(?([0-9]+)\)?: error: in "([\w\/]+)": (.+)$/;
-    private readonly regexTestCaseFatalError = /^(.+):?\(?([0-9]+)\)?: fatal error: in "([\w\/]+)": (.+)$/;
+	private readonly regexTestCaseError = /^(.+?)(?::\(?|\()([0-9]+)\)?: error: in "([\w\/]+)": (.+)$/;
+	private readonly regexTestCaseFatalError = /^(.+?)(?::\(?|\()([0-9]+)\)?: fatal error: in "([\w\/]+)": (.+)$/;
 
     constructor(
         readonly testExeTestItemId: string,
